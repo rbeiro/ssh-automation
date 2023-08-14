2dd740b0de572be462856b70552e58c3a0ac2dc4
Gabriel <grsilva112@gmail.com>
2023-08-14 14:33:48 -0300

    first commit

---
+++ src/app/api/provisioning/route.ts
@@ -0,0 +1,133 @@
+import { Client } from "ssh2";
+import { NextResponse } from "next/server";
+
+interface BodyRequestParams {
+  serialNumber: string;
+  slotGPON: number;
+  PONport: number;
+  ONUposition: number;
+  QoSProfilePPPoE: string;
+  VLANClient: number;
+  clientNameOLT: string;
+  clientNameOLT2: string;
+}
+
+export async function POST(request: Request) {
+  //console.log(request);
+  if (request.method !== "POST") {
+    return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
+  }
+  const sshClient = new Client();
+  const { params }: { params: BodyRequestParams } = await request.json();
+  console.log(params);
+
+  sshClient // Connect to the SSH server
+    .connect({
+      host: process.env.SSH_HOST,
+      password: process.env.SSH_PASS,
+      username: process.env.SSH_USER,
+      port: Number(process.env.SSH_PORT),
+      algorithms: {
+        kex: [
+          "diffie-hellman-group1-sha1",
+          "diffie-hellman-group-exchange-sha1",
+          "diffie-hellman-group14-sha1",
+          "diffie-hellman-group-exchange-sha256",
+          "ecdh-sha2-nistp521",
+          "ecdh-sha2-nistp384",
+          "ecdh-sha2-nistp256",
+        ],
+        cipher: [
+          "aes128-ctr",
+          "aes192-ctr",
+          "aes256-ctr",
+          "aes128-gcm",
+          "aes128-gcm@openssh.com",
+          "aes256-gcm",
+          "aes256-gcm@openssh.com",
+          "aes256-cbc",
+          "aes192-cbc",
+          "aes128-cbc",
+          "blowfish-cbc",
+          "3des-cbc",
+          "arcfour256",
+          "arcfour128",
+          "cast128-cbc",
+          "arcfour",
+        ],
+      },
+    });
+
+  // Handle events when the connection is established
+
+  sshClient.on("ready", () => {
+    console.log("Connected via SSH!");
+
+    sshClient.shell(false, {}, (err, stream) => {
+      if (err) throw err;
+
+      stream
+        .on("close", () => {
+          console.log("Stream :: close");
+        })
+        .on("data", (data: string) => {
+          console.log("OUTPUT: " + data);
+
+          switch (data) {
+            case "end":
+              console.log("End command");
+              sshClient.end();
+            case "Completed.":
+              NextResponse;
+              return NextResponse.json({ message: "Sucesso" }, { status: 201 });
+
+            default:
+          }
+          if (data === "end") {
+            console.log("End command");
+            sshClient.end();
+          }
+        });
+
+      const ONTInterfaceBase = `1/1/${params.slotGPON}/${params.PONport}/${params.ONUposition}`;
+
+      stream.stdin.write(
+        `configure equipment ont interface ${ONTInterfaceBase} sernum ${params.serialNumber} sw-ver-pland disabled optics-hist enable\n`
+      );
+      stream.stdin.write(
+        `configure equipment ont interface ${ONTInterfaceBase} desc1 "${params.clientNameOLT}" admin-state up\n`
+      );
+      stream.stdin.write(
+        `configure equipment ont interface ${ONTInterfaceBase} desc2 "${params.clientNameOLT2}"\n`
+      );
+      stream.stdin.write(
+        `configure equipment ont slot ${ONTInterfaceBase}/14 plndnumdataports 1 plndnumvoiceports 0 planned-card-type veip admin-state up\n`
+      );
+      stream.stdin.write(
+        `configure qos           interface ${ONTInterfaceBase}/14/1 upstream-queue 0 bandwidth-profile name:${params.QoSProfilePPPoE}\n`
+      );
+      stream.stdin.write(
+        `configure interface port      uni:${ONTInterfaceBase}/14/1 admin-up\n`
+      );
+      stream.stdin.write(
+        `configure bridge             port ${ONTInterfaceBase}/14/1 max-unicast-mac 32 max-committed-mac 1\n`
+      );
+      stream.stdin.write(`vlan-id ${params.VLANClient} tag single-tagged\n`);
+      stream.stdin.write("exit\n");
+      stream.stdin.write("exit all\n");
+      stream.stdin.write("admin save\n");
+      stream.stdin.write("end");
+    });
+
+    // Now you can execute commands, transfer files, etc.
+  });
+
+  // Handle errors during the SSH connection process
+
+  sshClient.on("error", (err) => {
+    console.error(err);
+  });
+
+  return NextResponse.json({ data: "Chegou aqui" });
+}
+``;
---
+++ src/app/page.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.scss";
+import { useQuery } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { api } from "@/lib/axios";
+import { z } from "zod";
+
+import { Button, Input } from "@rbeiro-ui/react-components";
+import { useState } from "react";
+
+const provisioningSchema = z.object({
+  serialNumber: z.string(),
+  slotGPON: z.string(),
+  PONport: z.string(),
+  ONUposition: z.string(),
+  QoSProfilePPPoE: z.string(),
+  VLANClient: z.string(),
+  clientNameOLT: z.string(),
+  clientNameOLT2: z.string(),
+});
+
+type ProvisioningFormInput = z.input<typeof provisioningSchema>;
+
+export default function Home() {
+  const [status, setStatus] = useState("");
+  const {
+    register,
+    handleSubmit,
+    control,
+    watch,
+    formState: { isSubmitting, errors },
+  } = useForm<ProvisioningFormInput>({
+    resolver: zodResolver(provisioningSchema),
+  });
+  // const { data } = useQuery({
+  //   queryKey: ["create-provisioning"],
+  //   queryFn: async () =>
+  //     await api.post("/provisioning", {
+  //       params: {
+  //         serialNumber: "ALCL:FC31BF03",
+  //         slotGPON: 4,
+  //         PONport: 16,
+  //         ONUposition: 2,
+  //         QoSProfilePPPoE: "HSI_1G_UP",
+  //         VLANClient: 10,
+  //         clientNameOLT: "TESTE NOKIA AUTO",
+  //         clientNameOLT2: "TESTE NOKIA AUTO",
+  //       },
+  //     }),
+  // });
+  console.log(errors);
+
+  async function handleProvising(data: ProvisioningFormInput) {
+    console.log(data);
+    console.log(errors);
+    api
+      .post("/provisioning", {
+        params: data,
+      })
+      .then((data) => {
+        if (data.status == 200) {
+          setStatus("ok");
+        }
+      });
+  }
+  return (
+    <main className={styles.main}>
+      <form className={styles.form} onSubmit={handleSubmit(handleProvising)}>
+        <Input labelName="Serial Number da ONT" {...register("serialNumber")} />
+        <Input labelName="Slot GPON" {...register("slotGPON")} />
+        <Input labelName="Porta PON" {...register("PONport")} />
+        <Input labelName="Posição da ONU" {...register("ONUposition")} />
+        <Input
+          labelName="Perfil de banda QoS Upstream PPoE"
+          {...register("QoSProfilePPPoE")}
+        />
+        <Input labelName="VLAN Client PPPoE" {...register("VLANClient")} />
+        <Input
+          labelName="Descrição para Identificação do Assinante na OLT"
+          {...register("clientNameOLT")}
+        />
+        <Input
+          labelName="Descrição para Identificação do Assinante na CTO e/ou 2ª descrição"
+          {...register("clientNameOLT2")}
+        />
+        <div>
+          <Button type="submit">Provisionar</Button>
+          {status && status}
+        </div>
+      </form>
+    </main>
+  );
+}
